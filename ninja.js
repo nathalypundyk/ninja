8ea557a38cd2baa55887b6248f90c938a9340486
Nathaly <nathalypundyk@gmail.com>
2024-06-28 10:22:02 -0400

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -22,6 +22,23 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(nombre) {
+        super(nombre, 200);
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+console.log("......................")
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
